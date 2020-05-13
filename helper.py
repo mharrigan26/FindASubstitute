13b870fc47bb3dbd0c2b5ba66d88bb326b9546c4
“Alexandra” <abullensmith>
2020-05-13 15:47:57 -0400

    quick fix

--- helper.py
+++ helper.py
@@ -7,16 +7,16 @@ def getAllEmployees(conn):
     data = curs.fetchall()
     return data
 
-def shiftExists(conn,permanent,day,time,employee):
+def shiftExists(conn,permanent,day,time,endtime,employee):
     curs = dbi.dict_cursor(conn)
-    curs.execute('''select * from shift1 where day = %s and time = %s and employee = %s''', (day,time,employee))
+    curs.execute('''select * from shift1 where day = %s and time = %s and endtime = %s and employee = %s''', (day,time,endtime,employee))
     data = curs.fetchall()
     return data
 
-def insertShift(conn,permanent,day,time,employee):
+def insertShift(conn,permanent,day,time,endtime,employee):
     curs = dbi.dict_cursor(conn)
-    curs.execute('''insert into shift1(permanent,day,time,employee) values(%s,%s,%s,%s)''',
-        (permanent,day,time,employee))
+    curs.execute('''insert into shift1(permanent,day,time,endtime,employee) values(%s,%s,%s,%s,%s)''',
+        (permanent,day,time,endtime,employee))
     conn.commit()
     
     
